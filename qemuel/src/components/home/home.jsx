3d266dae075691580667c06e0125beec151c6291
Yiğit Alsancak <yigitalsancak8@gmail.com>
2022-12-02 12:26:37 +0300

    Added navbar

--- qemuel/src/components/home/home.jsx
+++ qemuel/src/components/home/home.jsx
@@ -6,11 +6,18 @@ class Home extends Component {
     state = {  } 
     render() { 
         return (
-            <div id="home-box">
+            <div id="home">
+            <ul id='navbar'>
+                <li className='navbar-item'>Home</li>
+                <li className='navbar-item'>Projects</li>
+                <li className='navbar-item'>About Me</li>
+            </ul>
+
+                <h1> I am a Web Developer</h1>
                 <div id='img-container'>
                     <img id='my-image' src={myImage}></img>
                     <div>
-                        <p> Welcome to Qemuel.dev!</p>
+
                     </div>
                 </div>
             </div>
